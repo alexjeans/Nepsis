a061df11f85583a02a14b4aa02dc4b2e5896708d
alexsilvauam <silvaajc@uamv.edu.ni>
2026-05-02 22:54:15 -0600

    feat: agregar HistoryScreen
    todas las pantallas anteriores son PlaceHolders

--- app/src/main/java/com/example/nepsis/presentation/history/HistoryScreen.kt
+++ app/src/main/java/com/example/nepsis/presentation/history/HistoryScreen.kt
@@ -7,14 +7,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 
 @Composable
-fun ProfileScreen() {
+
+fun HomeScreen(
+    navController: NavController
+){
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Pantalla de Perfil")
+        Text(text = "Pantalla de Historial")
     }
 }
