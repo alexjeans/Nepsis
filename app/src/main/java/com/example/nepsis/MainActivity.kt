2c6a0c70ff277d0b64b4dfb3db2e8550bc301d56
alexsilvauam <silvaajc@uamv.edu.ni>
2026-05-02 22:31:03 -0600

    feat: hacer host inicial
    Main Activity -> NepsisApp, manda a llamarlo, y de ahi se crea la navegacion.
    
    -> NepsisApp() crea el navController
    -> NepsisApp() llama a AppNavHost
    -> AppNavHost decide qué pantalla mostrar

--- app/src/main/java/com/example/nepsis/MainActivity.kt
+++ app/src/main/java/com/example/nepsis/MainActivity.kt
@@ -3,45 +3,13 @@ package com.example.nepsis
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.nepsis.ui.theme.NepsisTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-            NepsisTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            NepsisApp()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NepsisTheme {
-        Greeting("Android")
     }
 }
