3a0d1c0b7b9036a3a6c7cae96b9d708fdb534583
Elian <eolivares@uamv.edu.ni>
2026-05-03 19:06:20 -0600

    la login screen ya hecha tuani, con sus validaciones y theme

--- app/src/main/java/com/example/nepsis/NepsisApp.kt
+++ app/src/main/java/com/example/nepsis/NepsisApp.kt
@@ -4,9 +4,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.nepsis.navigation.AppDestinations
 import com.example.nepsis.navigation.AppNavHost
 import com.example.nepsis.ui.components.NepsisBottomBar
+
 
 // Este archivo es como el visual de la app, No representa pantalla especifica
 // Se arma la estructura global del UI
@@ -16,8 +19,12 @@ fun NepsisApp(){
     val navController = rememberNavController()
     AppNavHost(navController = navController)
 
+    val navBackStackEntry = navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry.value?.destination?.route
+
     Scaffold(
         bottomBar = {
+            if (currentRoute!= AppDestinations.LOGIN)
             NepsisBottomBar(navController = navController)
         }
     ) { innerPadding ->
